1232fc4cbcccba33b47ce8fdd18adb103a38a8a3
Hugo Soares <hugosoareslopes1@gmail.com>
2026-04-15 10:49:31 -0300

    fix: Add the @Column tag to the class.

--- src/main/java/dev/ghou/CadastroDePessoas/People/PersonModel.java
+++ src/main/java/dev/ghou/CadastroDePessoas/People/PersonModel.java
@@ -14,10 +14,13 @@ public class PersonModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
 
+    @Column (name = "name")
     private String name;
 
+    @Column (name = "age")
     private int age;
 
     @Column(unique = true)
