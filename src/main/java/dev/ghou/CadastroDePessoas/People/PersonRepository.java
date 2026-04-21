3ce679356348d0e7d20376010ea5bf5592dec447
Hugo Soares <hugosoareslopes1@gmail.com>
2026-04-21 16:51:46 -0300

    Build: Conencta o BD H2 a minha aplicação.

--- src/main/java/dev/ghou/CadastroDePessoas/People/PersonRepository.java
+++ src/main/java/dev/ghou/CadastroDePessoas/People/PersonRepository.java
@@ -1,4 +1,7 @@
 package dev.ghou.CadastroDePessoas.People;
 
-public class PersonRepository {
+import dev.ghou.CadastroDePessoas.Trip.TripModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<PersonModel, Long> {
 }
