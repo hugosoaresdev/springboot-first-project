1e482ec32545fe534dfdca758d5874736a7bbee4
Hugo Soares <hugosoareslopes1@gmail.com>
2026-05-14 20:48:39 -0300

    feat: Update the controller with mock data

--- src/main/java/dev/ghou/CadastroDePessoas/People/PersonController.java
+++ src/main/java/dev/ghou/CadastroDePessoas/People/PersonController.java
@@ -1,8 +1,6 @@
 package dev.ghou.CadastroDePessoas.People;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class PersonController {
     @GetMapping("/hello")
     public String helloWorld(){
         return "Te amo bem";
+    }
+
+    // Adicionar Pessoa (Create)
+    @PostMapping("/add")
+    public String addPessoa(){
+        return "Pessoa adicionada";
+    }
+
+    // Mostrar Pessoa por ID (Read)
+    @GetMapping("/mostrarPessoa")
+    public String mostrarPessoa(){
+        return "Mostrar Pessoa por ID";
+    }
+
+    // Mostrar todas as Pessoas (Read)
+    @GetMapping("/mostrarTodos")
+    public String mostrarTodos() {
+        return "Mostrar todas as Pessoas";
+    }
+
+    // Alterar dados das Pessoas (Update)
+    @PutMapping("/alterar")
+    public String alterarPessoa(){
+        return "Pessoa Alterada ID";
+    }
+
+    // Deletar Pessoa (Delete)
+    @DeleteMapping("/deletar")
+    public String deletarPessoa() {
+        return "Pessoa Deletada por ID";
     }
 }
