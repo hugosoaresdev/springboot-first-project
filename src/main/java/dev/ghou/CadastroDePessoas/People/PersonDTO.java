9d5ef36b130e1929eb44fdcd0ef637e0f9e6c5b5
Hugo Soares <hugosoareslopes1@gmail.com>
2026-07-16 16:49:12 -0300

    chore: inclui DTO pela entidade e mapeia manualmente no PersonMapper os caminhos entidadexDTO

--- src/main/java/dev/ghou/CadastroDePessoas/People/PersonDTO.java
+++ src/main/java/dev/ghou/CadastroDePessoas/People/PersonDTO.java
@@ -1,4 +1,17 @@
 package dev.ghou.CadastroDePessoas.People;
 
+import dev.ghou.CadastroDePessoas.Trip.TripModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDTO {
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+    private TripModel trip;
 }
--- src/main/java/dev/ghou/CadastroDePessoas/People/PersonMapper.java
+++ src/main/java/dev/ghou/CadastroDePessoas/People/PersonMapper.java
@@ -1,4 +1,29 @@
 package dev.ghou.CadastroDePessoas.People;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PersonMapper {
+
+    public PersonModel map(PersonDTO personDTO){
+        PersonModel person = new PersonModel();
+        person.setId(personDTO.getId());
+        person.setAge(personDTO.getAge());
+        person.setEmail(personDTO.getEmail());
+        person.setName(personDTO.getName());
+        person.setTrip(personDTO.getTrip());
+
+        return person;
+    }
+
+    public PersonDTO map(PersonModel personModel){
+        PersonDTO person = new PersonDTO();
+        person.setId(personModel.getId());
+        person.setAge(personModel.getAge());
+        person.setEmail(personModel.getEmail());
+        person.setName(personModel.getName());
+        person.setTrip(personModel.getTrip());
+
+        return person;
+    }
 }
